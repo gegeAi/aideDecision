45cbff23d1a690b504223ea3f2b81bb0ba7c520a
unknown <frobion@if501-213-08.insa-lyon.fr>
2017-02-08 09:08:38 +0100

    debut calcul benefice

---
+++ Scripts/benefice.m
@@ -0,0 +1,9 @@
+function [Benefice] = benefice()
+    TempsProdParMachine = tempsMachine();
+    PrixVente = prixVente();
+    PrixAchatMatierePremieres = prixAchatMatierePremiere();
+    QuantiteMatierePremiereProd = quantite();
+    CoutHoraireMachine = coutHoraire();
+    
+    
+end

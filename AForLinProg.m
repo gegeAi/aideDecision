6564688174250bd53e92ba92d2351f27bfaacd3b
unknown <fteppe@if501-213-10.insa-lyon.fr>
2017-02-08 09:13:20 +0100

    constains

---
+++ AForLinProg.m
@@ -0,0 +1,17 @@
+function [ A ] = AForLinProg()
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+A = [ 18 5 0 5 0 10;
+      17 2 12 15 7 12;
+      8 1 11 0 10 25;
+      2 10 5 4 13 7;
+      15 0 8 7 10 15;
+      5 5 3 12 8 6;
+      13 3 5 8 10 7;
+      1 2 1 2 0 4;
+      2 1 1 5 2 1;
+      
+
+end
+
---
+++ bForLinProg.m
@@ -0,0 +1,18 @@
+function [ b ] = bForLinProg()
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+    
+b = [4800;
+    4800;
+    4800;
+    4800;
+    4800;
+    4800;
+    4800;
+    350;
+    820;
+    485;
+    ];
+
+end
+

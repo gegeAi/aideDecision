53c7af13ed41fa3423d63289706322ae609532dd
unknown <fteppe@if501-213-10.insa-lyon.fr>
2017-02-08 09:37:55 +0100

    responsable atelier

---
+++ Scripts/functionResponsableAtelier.m
@@ -0,0 +1,12 @@
+function [ f ] = functionResponsableAtelier()
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+
+f = [ -1;
+      -1;
+      -1;
+      -1;
+      -1;
+      -1];
+end
+
